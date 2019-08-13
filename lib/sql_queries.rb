43a69eae79007b31d8c5d3ef82b4da64cb421359
Nicole Dow <nikki.dow71@gmail.com>
2019-08-13 21:08:04 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -52,7 +52,7 @@ WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT projects.category, pledges.amount
+  "SELECT projects.category, SUM(pledges.amount)
 FROM projects
 JOIN pledges
 ON projects.id = pledges.project_id
