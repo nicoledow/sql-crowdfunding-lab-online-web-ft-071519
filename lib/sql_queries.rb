a1261b16a80c1727b6c51089babf8f7a935c0903
Nicole Dow <nikki.dow71@gmail.com>
2019-08-13 20:51:11 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,7 @@ FROM projects
 JOIN pledges
 ON projects.id = pledges.project_id
 GROUP BY pledges.project_id
-HAVING SUM(pledges.amount) > projects.funding_goal;"
+HAVING SUM(pledges.amount) >= projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
